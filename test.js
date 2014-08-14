5f6f83d674eaced87dde23476b84ebbf14c75174
morganherlocker <morgan.herlocker@gmail.com>
2014-08-14 10:45:21 -0400

    stub tests

---
+++ test.js
@@ -0,0 +1,37 @@
+var test = require('tape'),
+    tile = require('./');
+
+test('tile to geojson', function(t){
+  t.fail('not implmented');
+  t.end();
+});
+
+test('get parent', function(t){
+  t.fail('not implmented');
+  t.end();
+});
+
+test('get siblings', function(t){
+  t.fail('not implmented');
+  t.end();
+});
+
+test('has siblings', function(t){
+  t.fail('not implmented');
+  t.end();
+});
+
+test('has tile', function(t){
+  t.fail('not implmented');
+  t.end();
+});
+
+test('get quadkey', function(t){
+  t.fail('not implmented');
+  t.end();
+});
+
+test('quadkey to tile', function(t){
+  t.fail('not implmented');
+  t.end();
+});
