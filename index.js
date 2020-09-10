aa4c1aa7b940838576151fdbf140d46827b2e1f9
John Klancer <john.klancer@mapbox.com>
2020-09-10 10:34:49 -0400

    - fixes winding of GeoJSON to follow right-hand rule (#38)

--- index.js
+++ index.js
@@ -36,11 +36,11 @@ function tileToGeoJSON(tile) {
     var poly = {
         type: 'Polygon',
         coordinates: [[
-            [bbox[0], bbox[1]],
             [bbox[0], bbox[3]],
-            [bbox[2], bbox[3]],
+            [bbox[0], bbox[1]],
             [bbox[2], bbox[1]],
-            [bbox[0], bbox[1]]
+            [bbox[2], bbox[3]],
+            [bbox[0], bbox[3]]
         ]]
     };
     return poly;
